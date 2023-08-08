e1d7c4cc4b028f599af7c6f8c78ed97c37a1a0e2
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-08-08 12:40:08 +0200

    Add files via upload

---
+++ minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idumenil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/23 12:11:31 by idumenil          #+#    #+#             */
+/*   Updated: 2023/07/23 12:14:27 by idumenil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+#endif
