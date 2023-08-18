5fa6298d9a9d945f9f57d9104b91b515e7831aef
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-08-18 17:19:12 +0200

    Update server.c

--- server.c
+++ server.c
@@ -12,53 +12,52 @@
 
 #include "minitalk.h"
 
-pid_t   getpid(void);
-pid_t   g_clientpid = -1;
+pid_t	getpid(void);
+pid_t	g_clientpid = -1;
 
-void    recompose(int bit_value, int pid)
+void	recompose(int bit_value, int pid)
 {
-        int                             shift;
-        static int              bit_position = 7;
-        static char             received_char;
+	int				shift;
+	static int		bit_position = 7;
+	static char		received_char;
 
-        if (bit_position == -1 || pid != g_clientpid)
-        {
-                bit_position = 7;
-                received_char = 0;
-                g_clientpid = pid;
-        }
-        shift = 1 << (bit_position);
-        if (bit_value != 0)
-                received_char = (received_char | shift);
-        bit_position--;
-        if (bit_position == -1)
-                write (1, &received_char, 1);
+	if (bit_position == -1 || pid != g_clientpid)
+	{
+		bit_position = 7;
+		received_char = 0;
+		g_clientpid = pid;
+	}
+	shift = 1 << (bit_position);
+	if (bit_value != 0)
+		received_char = (received_char | shift);
+	bit_position--;
+	if (bit_position == -1)
+		write (1, &received_char, 1);
 }
 
-void    sig_handler(int sig, siginfo_t *info, void *ptr)
+void	sig_handler(int sig, siginfo_t *info, void *ptr)
 {
-        (void)ptr;
-        if (sig == SIGUSR1)
-                recompose(1, info->si_pid);
-        else
-                recompose(0, info->si_pid);
+	(void)ptr;
+	if (sig == SIGUSR1)
+		recompose(1, info->si_pid);
+	else
+		recompose(0, info->si_pid);
 }
 
-int     main(void)
+int	main(void)
 {
-        struct sigaction        action;
-        int                                     pid;
+	struct sigaction	action;
+	int					pid;
 
-        action.sa_flags = 0;
-        action.sa_sigaction = sig_handler;
-        sigaction(SIGUSR1, &action, 0);
-        sigaction(SIGUSR2, &action, 0);
-        pid = getpid();
-        write(1, "Server PID: ", 12);
-        ft_putnbr_fd(pid, 1);
-        write(1, "\n", 1);
-        while (1)
-                pause();
-        return (0);
+	action.sa_flags = 0;
+	action.sa_sigaction = sig_handler;
+	sigaction(SIGUSR1, &action, 0);
+	sigaction(SIGUSR2, &action, 0);
+	pid = getpid();
+	write(1, "Server PID: ", 12);
+	ft_putnbr_fd(pid, 1);
+	write(1, "\n", 1);
+	while (1)
+		pause();
+	return (0);
 }
-
